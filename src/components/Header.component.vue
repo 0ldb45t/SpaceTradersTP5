26be34253923bba094ccfc97bf39ea2a720c28cb
FestiveJoe <2187482@cegepgranby.qc.ca>
2025-12-07 17:44:52 -0500

    Header fini (kinda) + progrès contrat

--- src/components/Header.component.vue
+++ src/components/Header.component.vue
@@ -2,9 +2,12 @@
   <div class="entete mt-5 d-flex flex-row align-items-center justify-content-between">
     <h1 class="p-1 d-inline">TRADERS IN SPACE</h1>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     <div>
       <p>Test: {{ agent?.symbol }}</p>
 =======
+=======
+>>>>>>> Stashed changes
     <p class="bg-danger" v-if="statusData.serverResets !== undefined">
         Prochaine réinitalisation: {{ new Date(statusData.serverResets.next) }}
       </p>
@@ -14,6 +17,9 @@
       <p class="mb-0">Id: {{ agent.accountId }}</p>
       <!-- <p class="mb-0">Jeton: {{ agentToken }}</p> -->
        <!--J'ai mis la ligne du dessus en commentaire parce que c'est LAID-->
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
     </div>
   </div>
@@ -25,8 +31,11 @@ import { useAdminAgentStore } from '@/store';
 const store = useAdminAgentStore();
 let agent = store.agent;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 console.log(agent);
 =======
+=======
+>>>>>>> Stashed changes
 const agentToken = store.agentToken;
 const statusData = ref({});
 const TOKEN = store.TOKEN;
@@ -41,6 +50,9 @@ const getCurrentAccount = async () => {
         .then(response => response.json())
         .then(json => statusData.value = json);
 };
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 watch(
     () => store.agentToken,
