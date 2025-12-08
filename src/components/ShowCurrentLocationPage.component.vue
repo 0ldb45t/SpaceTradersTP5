36c3a819540140b076868546918cb13d4d29573d
0ldb45t <bontempsbastien@gmail.com>
2025-12-08 15:42:15 -0500

    fix div width in showCurrentLocationPage

--- src/components/ShowCurrentLocationPage.component.vue
+++ src/components/ShowCurrentLocationPage.component.vue
@@ -1,36 +1,40 @@
 <template>
-    <div class="d-flex flex-row w-100 justify-content-between align-items-start ">
-        <div class="d-flex flex-column justify-content-start w-25 m-3 mt-0 gap-3">
+    <div class="d-flex flex-row w-100 justify-content-around align-items-start">
+        <div class="d-flex flex-column justify-content-start w-25 m-3 mt-0 me-0 gap-3">
             <div v-if="agent?.symbol !== '' && readytoMap" class="p-3 align-self-end borderGreen">
                 <h4>Position de {{ agent.symbol }} : [ x: {{ shipPosition.x }}, y: {{ shipPosition.y }} ]</h4>
                 <h4 class="p-0 m-0">Symbole du systeme: {{ shipPosition?.systemSymbol }}</h4>
                 <router-link :to="{ name: 'Contrats' }">
-                    <button>Afficher les contrats</button>
+                    <button class="buttonDetails">Afficher les contrats</button>
                 </router-link>
-                <div v-for="trait in shipPosition.traits">
+                <hr class="borderGreen ms-5 me-5" />
+                <div v-for="trait, i in shipPosition.traits">
                     <p class="text-end textMid">{ Symbole: {{ trait.name }} }</p>
                     <p class="text-start textMid"> Description: {{ trait.description }} </p>
-                    <hr class="borderGreen ms-5 me-5" />
+                    <hr v-if="i < shipPosition.traits.length - 1" class="borderGreen ms-5 me-5" />
                 </div>
             </div>
         </div>
-        <div class="data p-5 text-center flex-column borderGreen w-25 me-3" v-if="cell.length > 0">
-            <strong>
-                <h4>
-                    Vous visez :
-                </h4>
-                <div v-for="(item, i) in cell">
-                    <p v-if="i === 0" :class="{ vousEtesIci: item.vousEtesIci }">{{ item.vousEtesIci }}</p>
-                    <p>
-                        {{ item.symbol }} : {{ item.type }} :
-                    </p>
-                    <p>
-                        [x:{{ item.x }}, y:{{ item.y }}]
-                    </p>
-                </div>
-            </strong>
+        <div class="w-25 d-flex flex-column">
+            <div class="borderGreen w-100"></div>
+            <div class="data text-center flex-column borderGreen w-100" v-if="cell.length > 0">
+                <strong>
+                    <h4>
+                        Vous visez :
+                    </h4>
+                    <div v-for="(item, i) in cell">
+                        <p v-if="i === 0" :class="{ vousEtesIci: item.vousEtesIci }">{{ item.vousEtesIci }}</p>
+                        <p>
+                            {{ item.symbol }} : {{ item.type }} :
+                        </p>
+                        <p>
+                            [x:{{ item.x }}, y:{{ item.y }}]
+                        </p>
+                    </div>
+                </strong>
+            </div>
         </div>
-        <div class=" d-flex justify-content-center align-items-center mr-3 w-50 flex-shrink-0">
+        <div class="d-flex justify-content-center align-items-center flex-shrink-0 m-0 p-0">
             <MapComponent v-if="readytoMap" />
         </div>
 
@@ -64,6 +68,11 @@ watch(
     color: red;
 }
 
+.buttonDetails {
+    border: 2px solid #3cff00;
+    background-color: green;
+    color: #3cff00;
+}
 .borderGreen {
     border: 2px solid green;
 }
